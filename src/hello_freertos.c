df209aaccb56793f8bdf5b00d96b0db98e037e59
Ashton Snelgrove <ashton@snelgrove.science>
2024-09-16 11:49:30 -0600

    Update template with latest fixes from rtos.02 reference.

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -1,51 +1,54 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
+
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 2UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
+int count = 0;
+bool on = false;
 
+#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-int counter;
-bool led_is_on;
-
-void blink_task(__unused void *params)
-{
-    counter = 0;
-    led_is_on = false;
+void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
     while (true) {
-	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_is_on);
-	if (counter++ % 47)
-	    led_is_on = !led_is_on;
-        vTaskDelay(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+        if (count++ % 11) on = !on;
+        vTaskDelay(500);
     }
 }
 
-
-void main_task(__unused void *params)
-{
-    xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    int count = 0;
+void main_task(__unused void *params) {
+    xTaskCreate(blink_task, "BlinkThread",
+                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
     char c;
     while(c = getchar()) {
-	if (c <= 'z' && c >= 'a') putchar(c - 32);
-	else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-	else putchar(c);
+        if (c <= 'z' && c >= 'a') putchar(c - 32);
+        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
+        else putchar(c);
     }
 }
 
 int main( void )
 {
     stdio_init_all();
+    const char *rtos_name;
+    rtos_name = "FreeRTOS";
     TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    xTaskCreate(main_task, "MainThread",
+                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
     vTaskStartScheduler();
     return 0;
 }
